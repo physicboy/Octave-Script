fda5ebaf869bff3b4e7ebec88e549f8512ef87bd
physicboy <64262286+physicboy@users.noreply.github.com>
2025-02-14 17:10:19 +0800

    compare the spectrum of gaussion random for different sampling period

---
+++ compare_random_fft.m
@@ -0,0 +1,32 @@
+tt = 0:1e-6:1e-3;
+rr = 0.4*randn(size(tt));
+
+ftt = fft(rr)/length(rr);
+ftt = ftt(1:floor(length(ftt)/2));
+ftt(2:length(ftt)) = 2*ftt(2:length(ftt));
+freqtt = (0:length(ftt)-1)*0.5*1/(tt(2) - tt(1))/length(ftt);
+
+subplot(2,1,1),loglog(freqtt,abs(ftt));hold on;
+subplot(2,1,2),plot(tt,rr);hold on;
+
+tt = 0:1e-6/2:1e-3;
+rr = 0.4*randn(size(tt));
+
+ftt = fft(rr)/length(rr);
+ftt = ftt(1:floor(length(ftt)/2));
+ftt(2:length(ftt)) = 2*ftt(2:length(ftt));
+freqtt = (0:length(ftt)-1)*0.5*1/(tt(2) - tt(1))/length(ftt);
+
+subplot(2,1,1),loglog(freqtt,abs(ftt));hold on;
+subplot(2,1,2),plot(tt,rr);hold on;
+
+tt = 0:1e-6/4:1e-3;
+rr = 0.4*randn(size(tt));
+
+ftt = fft(rr)/length(rr);
+ftt = ftt(1:floor(length(ftt)/2));
+ftt(2:length(ftt)) = 2*ftt(2:length(ftt));
+freqtt = (0:length(ftt)-1)*0.5*1/(tt(2) - tt(1))/length(ftt);
+
+subplot(2,1,1),loglog(freqtt,abs(ftt));grid on;hold off;
+subplot(2,1,2),plot(tt,rr);grid on;hold off
