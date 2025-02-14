6906a87654c9ea7f940ca0da19d9275f06b456c3
physicboy <64262286+physicboy@users.noreply.github.com>
2025-02-14 17:12:53 +0800

    Update compare_random_fft.m

--- compare_random_fft.m
+++ compare_random_fft.m
@@ -30,3 +30,11 @@ freqtt = (0:length(ftt)-1)*0.5*1/(tt(2) - tt(1))/length(ftt);
 
 subplot(2,1,1),loglog(freqtt,abs(ftt));grid on;hold off;
 subplot(2,1,2),plot(tt,rr);grid on;hold off
+
+subplot(2,1,1)
+ylabel('Absolute Amplitude')
+xlabel('Freq [Hz]')
+
+subplot(2,1,2)
+ylabel('time series')
+xlabel('Time [sec]')
